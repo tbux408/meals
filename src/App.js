194916fff191ee6eb4d29955309d63ab4b1b5df2
Tyler <tbux@vt.edu>
2025-02-14 11:33:59 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <h1>Studio 249</h1> */}
+
+        <a href="https://www.instagram.com/studio249__/">
+          <img
+            src={
+              "/434901950_443213378263818_1248838208233989345_n-removebg-preview.png"
+            }
+            alt="studio 249"
+            title="instagram"
+            className={"App-logo"}
+          ></img>
+        </a>
+        <h4>comming soon...</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
