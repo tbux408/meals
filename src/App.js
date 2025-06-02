3778bf71daa2b09b41bd9a8c2db67ce94ad8e591
Tyler <tbux@vt.edu>
2025-06-02 10:16:15 -0400

    meal update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from "./logo.svg";
+import logo from "./434901950_443213378263818_1248838208233989345_n-removebg-preview.png";
 import "./App.css";
+import React, { useEffect, useState } from "react";
+import CircleIcon from "@mui/icons-material/Circle";
+import { login, get_meals } from "./fetch/fetch";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <h1>Studio 249</h1> */}
+  const [passcode, setPasscode] = useState("");
+  const [error, setError] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(true);
 
-        <a href="https://www.instagram.com/studio249__/">
-          <img
-            src={
-              "/434901950_443213378263818_1248838208233989345_n-removebg-preview.png"
-            }
-            alt="studio 249"
-            title="instagram"
-            className={"App-logo"}
-          ></img>
-        </a>
-        <h4>comming soon...</h4>
-      </header>
+  useEffect(() => {
+    const access_token = localStorage.getItem("access_token_meals");
+    if (access_token) {
+      setLoggedIn(true);
+    }
+    get_meals("/meals", access_token, setLoggedIn);
+  }, []);
+
+  const handleChange = async (e) => {
+    const value = e.target.value;
+    if (value.length <= 6) {
+      setPasscode(value);
+      setError(false);
+    }
+    if (value.length === 6) {
+      const response = await login(value);
+      console.log(response);
+      if (response.status === 200) {
+        const data = await response.json();
+        localStorage.setItem("access_token_meals", data);
+        console.log(data);
+      } else {
+        console.log("Login failed");
+        setError(true);
+      }
+    }
+  };
+
+  return (
+    <div>
+      {!loggedIn ? (
+        <div
+          className="body"
+          style={{ backgroundColor: error ? "rgb(186, 50, 50)" : "#282c34" }}
+        >
+          <div className="passcode-box">
+            <input
+              type="password"
+              maxLength={6}
+              className="passcode"
+              value={passcode}
+              onChange={handleChange}
+              id="passcode"
+              name="passcode"
+            />
+          </div>
+          <div className="digit-box">
+            {Array.from({ length: 6 }, (_, index) => (
+              <p className="digit" key={index}>
+                {index < passcode.length && (
+                  <CircleIcon sx={{ width: "100%" }} />
+                )}
+              </p>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div> logged </div>
+      )}
     </div>
   );
 }
---
+++ src/fetch/fetch.js
@@ -0,0 +1,50 @@
+export const login = async (passcode) => {
+  const response = await fetch(
+    process.env.REACT_APP_PUBLIC_API_URL + "/login",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // Authorization: passcode,
+      },
+      body: JSON.stringify({ passcode: passcode }),
+    }
+  );
+  // const data = await response.json();
+  return response;
+};
+
+
+export const get_meals = async (path, auth, setLoggedIn) => {
+  const response = await fetch(process.env.REACT_APP_PUBLIC_API_URL + path, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: auth,
+    },
+    // credentials: "include",
+    // body: JSON.stringify(body),
+  });
+  if (response.status === 401) {
+    setLoggedIn(false);
+    localStorage.removeItem("access_token_meals");
+    return null;
+  }
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
+
+export const fetch_meals = async (type, path, body) => {
+  const response = await fetch(process.env.REACT_APP_PUBLIC_API_URL + path, {
+    method: type,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "483282",
+    },
+    // credentials: "include",
+    // body: JSON.stringify(body),
+  });
+  const data = await response.json();
+  return data;
+};
